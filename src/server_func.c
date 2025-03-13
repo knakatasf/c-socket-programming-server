0601579f70c25b31a4a4f7e012b549324da38859
kanaktasf <knakatasf@dons.usfca.edu>
2025-03-13 16:53:07 -0700

    Documented

--- src/server_func.c
+++ src/server_func.c
@@ -2,15 +2,15 @@
 #include "../include/config.h"
 
 /*
- *  Creates a welcoming socket, specifying the server's port.
- *  Waits for a client request.
- *  Upon acceptance of the client request, pass the client socket. 
+ *  Creates a welcoming socket, specifying the server's port. Waits for a client request.
+ *  Upon acceptance of the client request, open a client socket; return both sockets.
  */
 struct socket_pair open_client_tcp_socket(const int server_port)
 {
     struct socket_pair socks;
     socks.server_sock = socket(PF_INET, SOCK_STREAM, 0);
 
+    /* Need this so the server socket can quickly reuse the same address and port */
     int opt = 1;
     if (setsockopt(socks.server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
     {
@@ -19,12 +19,14 @@ struct socket_pair open_client_tcp_socket(const int server_port)
         exit(EXIT_FAILURE);
     }
     
+    /* Prepares the server's address and port */
     struct sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(server_port);
     
+    /* Binds the socket with the specified port */
     if (bind(socks.server_sock, (struct sockaddr*)&sin, sizeof(sin)) < 0)
     {
         perror("Couldn't bind socket to the address");
@@ -39,24 +41,25 @@ struct socket_pair open_client_tcp_socket(const int server_port)
         exit(EXIT_FAILURE);
     }
     
-    printf("Server listening to port %d...\n", server_port);
+    //printf("Server listening to port %d...\n", server_port);
     
     struct sockaddr_in addr;
     socklen_t addr_len = sizeof(addr);
-    socks.client_sock = accept(socks.server_sock, (struct sockaddr*)&addr, &addr_len); // Populates client's addr and return the client socket.
-    
+    socks.client_sock = accept(socks.server_sock, (struct sockaddr*)&addr, &addr_len);
     if (socks.client_sock < 0)
     {
         perror("Error accepting connection");
         close(socks.server_sock);
         exit(EXIT_FAILURE);
     }
-    
-    printf("Client connected!\n");
-    
+    //printf("Client connected!\n");
     return socks;
 }
 
+/* 
+ * Receives the json data from the client for the configuration
+ * Calls load_config so that the program can access environment variables
+ */
 void receive_json_via_tcp(const int server_port)
 {
     struct socket_pair socks = open_client_tcp_socket(server_port);
@@ -68,9 +71,8 @@ void receive_json_via_tcp(const int server_port)
     else
     {
         buffer[byte_received] = '\0';
-        printf("Received json data:\n%s", buffer);
-        load_config(buffer);
-
+        // printf("Received json data:\n%s", buffer);
+        load_config(buffer); // Populate the environment variables
     }
     close(socks.client_sock);
     close(socks.server_sock);
@@ -81,16 +83,15 @@ long receive_packet_trains(const int server_port, const int num_of_packets, cons
     struct sockaddr_in server_addr, client_addr;
     int sock = socket(PF_INET, SOCK_DGRAM, 0);
 
-    int receive_buffer_size = 256 * 1024 * 1024; // 256 MB
-    if (setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &receive_buffer_size, sizeof(receive_buffer_size)) < 0) // Sets the socket size enough to receive the two trains.
+    int receive_buffer_size = 1024 * 1024 * 1024; // 1024 MB; Sets the socket size enough to receive the two trains.
+    if (setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &receive_buffer_size, sizeof(receive_buffer_size)) < 0)
     {
         perror("Failed to set receive buffer size");
     }
 
     /*
-     *  recvfrom() will block until receiving a packet.
-     *  So, we want to set the socket's timeout.
-     *  For a certain time, if the socket doesn't receive any packet, stop waiting.
+     * recvfrom() will block until receiving a packet; we want to set the socket's timeout.
+     * For a certain time, if the socket doesn't receive any packet, stop waiting.
      */
     struct timeval timeout;
     timeout.tv_sec = (INTER_MEASUREMENT_TIME / 1000000) - 5; // 10 secs
@@ -106,34 +107,44 @@ long receive_packet_trains(const int server_port, const int num_of_packets, cons
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(server_port);
-    if (bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) // We need to specify the server's port for the trains.
+    if (bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) // Specifies the server's port
     {
         perror("Bind failed");
         close(sock);
         exit(EXIT_FAILURE);
     }
         
-    printf("Server is listening on port %d...\n", server_port);
+    //printf("Server is listening on port %d...\n", server_port);
 
-    char* buffer = malloc(num_of_packets * packet_size * 8); // Allocates enough buffer to receive the trains.
-    if (!buffer) {
+    /* Allocates enough buffer to receive the second train */
+    char* first_buffer = malloc(num_of_packets * packet_size * 32);
+    if (!first_buffer) {
         perror("Failed to allocate buffer");
         close(sock);
         exit(EXIT_FAILURE);
     }
 
-    long first_train_time_duration = receive_packet_train(sock, buffer, packet_size, &client_addr);
-    long second_train_time_duration = receive_packet_train(sock, buffer, packet_size, &client_addr);
+    /* Allocates enough buffer to receive the second train */
+    char* second_buffer = malloc(num_of_packets * packet_size * 32);
+    if (!second_buffer) {
+        perror("Failed to allocate buffer");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
 
-    free(buffer);
+    long first_train_time_duration = receive_packet_train(sock, first_buffer, packet_size, &client_addr);
+    long second_train_time_duration = receive_packet_train(sock, second_buffer, packet_size, &client_addr);
+
+    free(first_buffer);
+    free(second_buffer);
     
     close(sock);
-    printf("Finished receiving packet train\n");
 
-    if (first_train_time_duration != -1 && second_train_time_duration != -1) // Return the result only when the server receives BOTH trains.
+    /* Return the result only when the server receives BOTH trains */
+    if (first_train_time_duration != -1 && second_train_time_duration != -1)
     {
         long diff = labs(first_train_time_duration - second_train_time_duration);
-        printf("The difference of the reception time between first train and second train: %ld\n", diff);
+        //printf("The difference of the reception time between first train and second train: %ld\n", diff);
         return diff;
     }
     else
@@ -163,29 +174,27 @@ long receive_packet_train(const int sock, char buffer[], const int packet_size, 
             gettimeofday(&last_packet_time, NULL); // Always updates the last_packet_time.
             if (!first_packet_received) // If the socket haven't received any packet yet,
             {
-                printf("First packet of the train received.\n");
+                //printf("First packet of the train received.\n");
                 first_packet_received = 1;
                 first_packet_time = last_packet_time; // Sets the first_packet_time.
             }
         }
         else if (recv_size == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) // When timeout, break the loop.
         {
-            printf("Receiving packet train timeout. Received %d packets.\n", packet_count);
+            //printf("Receiving packet train timeout. Received %d packets.\n", packet_count);
             break;
         }
     }
 
     if (first_packet_received)
     {
-        /*
-         * Need to convert the duration to millisecond.
-         */
+        /* Need to convert the duration to millisecond */
         long train_duration = (last_packet_time.tv_sec - first_packet_time.tv_sec) * 1000 + 
                               (last_packet_time.tv_usec - first_packet_time.tv_usec) / 1000;
-        printf("Train reception duration: %ld ms. Packet count: %d\n", train_duration, packet_count);
+        // printf("Train reception duration: %ld ms. Packet count: %d\n", train_duration, packet_count);
         return train_duration;
     }
-    else // When the socket receives NO packets,
+    else // When the socket receives NO packets
     {
         printf("No packets received within the timeout.\n");
         return -1;
@@ -204,7 +213,7 @@ void send_result(const int server_port, const long result)
         close(socks.server_sock);
         exit(EXIT_FAILURE);
     }
-    printf("Sent result to the client.\n");
+    //printf("Sent result to the client.\n");
     close(socks.client_sock);
     close(socks.server_sock);
 }
