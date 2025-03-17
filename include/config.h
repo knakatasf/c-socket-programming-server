6ad6dab3bfb71de8db13b0ccb36596aae98b1471
kanaktasf <knakatasf@dons.usfca.edu>
2025-03-17 11:20:28 -0700

    Documented

--- include/config.h
+++ include/config.h
@@ -3,20 +3,20 @@
 
 #include <cjson/cJSON.h>
 
-extern int PRE_TCP_PORT; // 7777
-extern int POST_TCP_PORT; // 6666
-extern int CLIENT_UDP_PORT; //9876
-extern int SERVER_UDP_PORT; // 8765
+extern int PRE_TCP_PORT; // The server's port for pre-probing; default 7777
+extern int POST_TCP_PORT; // The server's port for post-probing; default 6666
+extern int CLIENT_UDP_PORT; // The client's port for probing; default 9876
+extern int SERVER_UDP_PORT; // The server's port for probing; default 8765
 
-extern int UNCORP_TCP_HEAD; // 9999
-extern int UNCORP_TCP_TAIL; // 8888
+extern int UNCORP_TCP_HEAD; // The server's port for the head SYN packet; default 9999
+extern int UNCORP_TCP_TAIL; // The server's port for the tail SYN packet; default 8888
 
-extern char SERVER_IP[17]; // "192.168.64.7"
+extern char SERVER_IP[17]; // The server's IP address; "192.168.64.7"
 
-extern int UDP_SIZE; // 1000B
+extern int UDP_SIZE; // The payload size of the packet in the packet train; default 1000B
 extern int INTER_MEASUREMENT_TIME; // 15 secs
-extern int NUM_OF_UDP_PACKETS; // 6000
-extern int TTL; // 255
+extern int NUM_OF_UDP_PACKETS; // The number of the packets in the packet train; default 6000
+extern int TTL; // The TTL for the udp packets in the train; default 255
 
 void load_config(const char*);
 
